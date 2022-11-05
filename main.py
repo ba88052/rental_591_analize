5bee6ed7429ac7232900052026f94bf8b5e33433
Sam_Liao <ba88052@gmail.com>
2022-11-06 01:20:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ def index():
     return "Hello!! Welcome to rental_591_analize"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8081, debug = True)
+    app.run(host="0.0.0.0", port=8081, debug = False)
