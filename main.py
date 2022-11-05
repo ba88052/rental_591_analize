d42720bc4f1e56287cac8fb8bac75474b4d1567f
Sam_Liao <ba88052@gmail.com>
2022-11-06 00:21:57 +0800

    change_port

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ def index():
     return "Hello!! Welcome to rental_591_analize"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug = True)
+    app.run(host="0.0.0.0", port=80, debug = True)
